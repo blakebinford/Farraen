502e1bc35c7a38b9455ef164830a9d9acbead607
blakebinford <binford.blake@gmail.com>
2025-11-13 15:15:51 +0000

    ui updates

--- pipeledger/settings.py
+++ pipeledger/settings.py
@@ -154,7 +154,11 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
